b4f40df33b57e389804d7c99ab8fc2af9408e591
EmelyChavez <00097923@uca.edu.sv>
2026-05-26 21:26:34 -0600

    :memo:: adding auth components

---
+++ WorkHive/src/main/java/me/workhive/workhive/service/impl/AuthServiceImpl.java
@@ -0,0 +1,144 @@
+package me.workhive.workhive.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import me.workhive.workhive.dto.request.CandidateRegisterRequest;
+import me.workhive.workhive.dto.request.LoginRequest;
+import me.workhive.workhive.dto.request.RecruiterRegisterRequest;
+import me.workhive.workhive.dto.response.AuthResponse;
+import me.workhive.workhive.model.CandidateProfile;
+import me.workhive.workhive.model.Company;
+import me.workhive.workhive.model.RecruiterProfile;
+import me.workhive.workhive.model.User;
+import me.workhive.workhive.model.enums.Role;
+import me.workhive.workhive.repository.CandidateRepository;
+import me.workhive.workhive.repository.CompanyRepository;
+import me.workhive.workhive.repository.RecruiterRepository;
+import me.workhive.workhive.repository.UserRepository;
+import me.workhive.workhive.service.AuthService;
+import me.workhive.workhive.util.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+    private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
+    private final RecruiterRepository recruiterProfileRepository;
+    private final CandidateRepository candidateProfileRepository;
+
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public AuthResponse registerCandidate(CandidateRegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = User.builder()
+                .name(request.getName())
+                .gender(request.getGender())
+                .email(request.getEmail())
+                .password(
+                        passwordEncoder.encode(request.getPassword())
+                )
+                .role(Role.CANDIDATE)
+                .build();
+
+        userRepository.save(user);
+
+        CandidateProfile candidateProfile = CandidateProfile.builder()
+                .user(user)
+                .build();
+
+        candidateProfileRepository.save(candidateProfile);
+
+        String token = jwtUtil.generateToken(user);
+
+        return new AuthResponse(
+                token,
+                user.getEmail(),
+                user.getRole(),
+                "Logged in successfully as " + user.getRole().name()
+        );
+    }
+
+    @Override
+    public AuthResponse registerRecruiter(RecruiterRegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        Company company;
+
+        if (request.getCompanyId() != null) {
+
+            company = companyRepository.findById(request.getCompanyId())
+                    .orElseThrow(() ->
+                    new RuntimeException("Company not found"));
+
+        } else {
+            company = Company.builder()
+                    .name(request.getCompanyName())
+                    .location(request.getLocation())
+                    .sector(request.getSector())
+                    .build();
+
+            companyRepository.save(company);
+        }
+
+        User user = User.builder()
+                .name(request.getName())
+                .gender(request.getGender())
+                .email(request.getEmail())
+                .password(
+                        passwordEncoder.encode(request.getPassword())
+                )
+                .role(Role.RECRUITER)
+                .build();
+
+        userRepository.save(user);
+
+        RecruiterProfile recruiterProfile =
+                RecruiterProfile.builder()
+                        .user(user)
+                        .company(company)
+                        .build();
+
+        recruiterProfileRepository.save(recruiterProfile);
+
+        String token = jwtUtil.generateToken(user);
+
+        return new AuthResponse(
+                token,
+                user.getEmail(),
+                user.getRole(),
+                "Logged in successfully as " + user.getRole().name()
+        );
+    }
+
+    @Override
+    public AuthResponse login(LoginRequest request) {
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("Invalid credentials"));
+
+        boolean isValidPassword = passwordEncoder.matches(request.getPassword(), user.getPassword());
+
+        if (!isValidPassword) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        String token = jwtUtil.generateToken(user);
+        return new AuthResponse(
+                token,
+                user.getEmail(),
+                user.getRole(),
+                "Logged in successfully as " + user.getRole().name()
+        );
+    }
+
+}
