b10468ce592340b468e2f0ad896e6ec7cd245b61
EmelyChavez <00097923@uca.edu.sv>
2026-05-28 20:23:35 -0600

    :sparkles:: add entities related with vacancy

---
+++ WorkHive/src/main/java/me/workhive/workhive/domain/entities/Requirement.java
@@ -0,0 +1,29 @@
+package me.workhive.workhive.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "requirements")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Requirement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "requirements")
+    private List<Vacancy> vacancies;
+
+}
