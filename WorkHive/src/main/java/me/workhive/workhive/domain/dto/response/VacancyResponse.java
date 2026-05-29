b6eddbf928434bed1a42b4c697f6c4e8fd92bd30
EmelyChavez <00097923@uca.edu.sv>
2026-05-28 20:27:51 -0600

    :sparkles:: add requirement and vacancy dto reponses

---
+++ WorkHive/src/main/java/me/workhive/workhive/domain/dto/response/VacancyResponse.java
@@ -0,0 +1,31 @@
+package me.workhive.workhive.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.workhive.workhive.domain.dto.request.RequirementSelection;
+import me.workhive.workhive.domain.entities.Company;
+import me.workhive.workhive.domain.entities.Requirement;
+import me.workhive.workhive.domain.entities.enums.Modality;
+import me.workhive.workhive.domain.entities.enums.VacancyStatus;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class VacancyResponse {
+    private UUID id;
+    private Company company;
+    private String title;
+    private String description;
+    private List<String> requirements;
+    private BigDecimal salary;
+    private Modality modality;
+    private VacancyStatus status;
+}
