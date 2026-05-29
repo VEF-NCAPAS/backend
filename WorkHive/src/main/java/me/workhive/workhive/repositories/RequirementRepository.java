8b55d6437d5bf85625a1f407191be838285a0ac6
EmelyChavez <00097923@uca.edu.sv>
2026-05-28 20:25:51 -0600

    :sparkles:: add requirement and vacancy repositories

---
+++ WorkHive/src/main/java/me/workhive/workhive/repositories/RequirementRepository.java
@@ -0,0 +1,9 @@
+package me.workhive.workhive.repositories;
+
+import me.workhive.workhive.domain.entities.Requirement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RequirementRepository extends JpaRepository<Requirement, UUID> {
+}
---
+++ WorkHive/src/main/java/me/workhive/workhive/repositories/VacancyRepository.java
@@ -0,0 +1,39 @@
+package me.workhive.workhive.repositories;
+
+import me.workhive.workhive.domain.entities.Company;
+import me.workhive.workhive.domain.entities.Vacancy;
+import me.workhive.workhive.domain.entities.enums.Modality;
+import me.workhive.workhive.domain.entities.enums.VacancyStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface VacancyRepository extends JpaRepository<Vacancy, UUID> {
+    Page<Vacancy> findByStatus(VacancyStatus status, Pageable pageable);
+
+    Page<Vacancy> findByTitleContainingIgnoreCaseAndStatus(String title, VacancyStatus status, Pageable pageable);
+
+    Page<Vacancy> findByModalityAndStatus(Modality modality, VacancyStatus status, Pageable pageable);
+
+    Page<Vacancy> findByTitleContainingIgnoreCaseAndModalityAndStatus(
+            String title,
+            Modality modality,
+            VacancyStatus status,
+            Pageable pageable
+    );
+
+    Page<Vacancy> findByCompany(Company company, Pageable pageable);
+
+    Page<Vacancy> findByCompanyAndTitleContainingIgnoreCase(Company company, String title, Pageable pageable);
+
+    Page<Vacancy> findByCompanyAndModality(Company company, Modality modality, Pageable pageable);
+
+    Page<Vacancy> findByCompanyAndTitleContainingIgnoreCaseAndModality(
+            Company company,
+            String title,
+            Modality modality,
+            Pageable pageable
+    );
+}
