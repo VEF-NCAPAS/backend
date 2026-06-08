e7618090bd23d4dfde4da5c88f2d24ebe5f3dbbf
EmelyChavez <00097923@uca.edu.sv>
2026-06-08 00:29:43 -0600

    :sparkles:: add application, interview and technical test services interfaces

---
+++ WorkHive/src/main/java/me/workhive/workhive/services/ApplicationService.java
@@ -0,0 +1,25 @@
+package me.workhive.workhive.services;
+
+import me.workhive.workhive.domain.dto.request.CreateApplicationRequest;
+import me.workhive.workhive.domain.dto.request.UpdateApplicationStatusRequest;
+import me.workhive.workhive.domain.dto.response.ApplicationResponse;
+import me.workhive.workhive.domain.dto.response.PageableResponse;
+import me.workhive.workhive.domain.entities.User;
+
+import java.util.UUID;
+
+public interface ApplicationService {
+    ApplicationResponse createApplication(CreateApplicationRequest request, User user);
+    ApplicationResponse getApplicationById(UUID id, User user);
+    PageableResponse<? extends ApplicationResponse> getAllApplications(
+            int page,
+            int size,
+            String sortBy,
+            String sortOrder,
+            User user
+    );
+
+    ApplicationResponse withdrawApplication(UUID id, User user);
+    ApplicationResponse reviewApplication(UUID id, User user);
+    ApplicationResponse updateApplicationStatus(UUID id, UpdateApplicationStatusRequest request, User user);
+}
