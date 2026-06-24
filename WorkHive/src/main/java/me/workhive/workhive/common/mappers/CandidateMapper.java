0b72a0b5607c49cd8981568ce205d9a40474204b
EmelyChavez <00097923@uca.edu.sv>
2026-06-23 22:02:22 -0600

    :memo:: add candidate mapper

---
+++ WorkHive/src/main/java/me/workhive/workhive/common/mappers/CandidateMapper.java
@@ -0,0 +1,35 @@
+package me.workhive.workhive.common.mappers;
+
+import me.workhive.workhive.domain.dto.request.CandidateScoreRequest;
+import me.workhive.workhive.domain.dto.response.CandidateScoreResponse;
+import me.workhive.workhive.domain.entities.Cv;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CandidateMapper {
+
+    public CandidateScoreRequest toSearchRequest(List<String> skills, List<String> languages,
+                                                 List<String> education, String location, Integer exp) {
+        return CandidateScoreRequest.builder()
+                .skills(skills)
+                .languages(languages)
+                .education(education != null && !education.isEmpty() ? education.get(0) : null)
+                .location(location)
+                .minimumExperience(exp)
+                .build();
+    }
+
+    public CandidateScoreResponse toCandidateScoreDto(Cv cv, double score) {
+        return CandidateScoreResponse.builder()
+                .cvId(cv.getId())
+                .candidateProfileId(cv.getCandidateProfile().getId())
+                .name(cv.getCandidateProfile().getUser().getName())
+                .email(cv.getCandidateProfile().getUser().getEmail())
+                .location(cv.getLocation())
+                .city(cv.getCity())
+                .score(score)
+                .build();
+    }
+}
