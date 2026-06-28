2c9270bb452d24b3278ae20ac125ea28ac845151
EmelyChavez <00097923@uca.edu.sv>
2026-06-28 03:32:48 -0600

    :fire:: remove dotenv to deploy

--- WorkHive/src/test/java/me/workhive/workhive/WorkHiveApplicationTests.java
+++ WorkHive/src/test/java/me/workhive/workhive/WorkHiveApplicationTests.java
@@ -1,24 +1,10 @@
 package me.workhive.workhive;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WorkHiveApplicationTests {
-
-    @BeforeAll
-    static void setup() {
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./")
-                .ignoreIfMalformed()
-                .ignoreIfMissing()
-                .load();
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
-    }
 
     @Test
     void contextLoads() {
